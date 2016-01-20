ba0606e020474f35f7fc199e38485398a049af27
shinylane <roblane09@gmail.com>
2016-01-19 21:37:59 -0800

    Simple working user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  has_secure_password validations: true
+  validates :email, :presence => true, :uniqueness => true,
+            format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 end
