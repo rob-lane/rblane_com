47c8e91f26898653bea50935f8011ceda1177885
shinylane <roblane09@gmail.com>
2016-02-11 20:21:27 -0800

    Split admin stylesheet into separate manifest so we are not loading on non admin pages.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,5 +3,5 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
-# Add additional assets to the asset load path
-Rails.application.config.assets.paths << Rails.root.join('app', 'templates')
+# Include admin manifest
+Rails.application.config.assets.precompile += %w(admin.css)
